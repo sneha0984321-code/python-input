96b8ba4bfe8520af06a46d0c5f8500f32122c772
Sneha Sharma <sneha.0984321@gmail.com>
2026-08-09 17:51:29 +0530

    Add files via upload

---
+++ Question1.py
@@ -0,0 +1,10 @@
+first =  int(input("Enter the first number: "))
+second =int(input("Enter the second number: "))
+addition = first + second
+print ("Addition: ", addition)
+subtraction = first - second
+print ("Subtraction: ", subtraction)
+multiplication = first * second
+print ("Multiplication: ", multiplication)
+division = first / second
+print ("Division: ", division)
---
+++ Question2.py
@@ -0,0 +1,4 @@
+name = input("Enter your name: ")
+lastname = input("Enter your last name: ")
+fullname = name + " " + lastname
+print ("Hello, ", fullname ,"! Welcome to the Python program")
